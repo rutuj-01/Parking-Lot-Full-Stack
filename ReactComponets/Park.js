f1acc0e2882e8921b550289c3ba3797bf8c850c0
rutuj-01 <bhakrerutuj@gmail.com>
2026-02-19 09:00:25 +0530

    removed comments

--- ReactComponets/Park.js
+++ ReactComponets/Park.js
@@ -4,7 +4,6 @@ import car from './car.jpg';
 
 class Park extends React.Component {
     // ERROR 1: Missing constructor/initial state check. 
-    // If render() triggers before axios finishes, this.state.count[0] will crash.
     state = {
         ticket_number: '',
         car_size: '',
@@ -13,14 +12,10 @@ class Park extends React.Component {
         count: null // Initialized to null to test NPE logic
     }
 
-    // ERROR 2: Deprecated Lifecycle Method. 
-    // componentWillMount is deprecated and can lead to side-effect bugs in React Fiber.
     componentWillMount() {
         axios.get("http://localhost:8080/getlatestcar/")
             .then((response) => {
                 // ERROR 3: Potential NPE. 
-                // If response.data is null or the server sends a 204 No Content, 
-                // accessing .ticketNumber will throw an Uncaught TypeError.
                 this.setState({
                     ticket_number: response.data.ticketNumber,
                     car_size: response.data.carSize,
@@ -45,20 +40,13 @@ class Park extends React.Component {
                     count: res.data,
                     parking_slot: pre
                 })
-                // ERROR 5: State Access Race Condition.
-                // console.log(this.state.count) right after setState is unreliable 
-                // because setState is asynchronous.
                 console.log(this.state.count) 
             })
             // ERROR 6: Unhandled Promise Rejection.
-            // No .catch() block for axios calls. If the server is down, 
             // the UI hangs and the console fills with errors.
     }
 
     close() {
-        // ERROR 7: Potential Crash.
-        // Accessing this.props.history without checking if the component is 
-        // properly wrapped in withRouter or a Route.
         this.props.history.push("/")
     }
 
